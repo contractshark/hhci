c691cd277a9aad08213b023a09112a97979ea717
sam bacha <sam@freighttrust.com>
2021-03-19 05:14:48 -0700

    fix: init

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,9 @@
-https://github.com/nomiclabs/hardhat/archive/hardhat-core-v2.0.11.tar.gz
+#!/bin/sh
+wget https://github.com/nomiclabs/hardhat/archive/hardhat-core-v2.0.11.tar.gz
+tar xvzf hardhat-core-v2.0.11.tar.gz
 mv hardhat-hardhat-core-v2.0.11/ hardhat-core
+cd hardhat-core
+npm install 
 ./node_modules/.bin/tsc --build packages/hardhat-shorthand
-packages/hardhat-shorthand/
+ls -l packages/hardhat-shorthand/
 cd hardhat-core/packages/hardhat-shorthand/
